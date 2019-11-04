b336fa2af13e3ddfd491eed04c43e36c13aec48a
Dowon Kang <dowon@tuta.io>
2019-11-04 15:31:54 +0900

    Format some fish functions
    Why:
    
    * indentations are off

--- .config/fish/functions/prepend_to_path.fish
+++ .config/fish/functions/prepend_to_path.fish
@@ -1,8 +1,8 @@
 function prepend_to_path
 	for i in $argv
-if not contains $i $PATH
-and test -d $i
-set -x PATH $i $PATH
-end
-end
+		if not contains $i $PATH
+			and test -d $i
+			set -x PATH $i $PATH
+		end
+	end
 end
--- .config/fish/functions/remove_from_path.fish
+++ .config/fish/functions/remove_from_path.fish
@@ -1,7 +1,7 @@
 function remove_from_path
 	for i in $argv
-if contains $i $PATH
-set -x PATH (string match -v $i $PATH)
-end
-end
+		if contains $i $PATH
+			set -x PATH (string match -v $i $PATH)
+		end
+	end
 end
