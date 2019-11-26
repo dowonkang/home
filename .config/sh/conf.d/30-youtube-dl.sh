a6de83b7f361df74643b088de42236dfbbb00581
Dowon Kang <dowon@tuta.io>
2019-11-26 16:39:54 +0900

    Add youtube-dl alias

---
+++ .config/sh/conf.d/30-youtube-dl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+if command -v youtube-dl >/dev/null; then
+	alias ytdl-mp3='youtube-dl -x --audio-format mp3 --embed-thumbnail -o '\''%(title)s.%(ext)s'\'''
+	alias ytdl-mp3ls='youtube-dl --yes-playlist -x --audio-format mp3 --embed-thumbnail -o '\''%(playlist)s/%(playlist_index)s %(title)s.%(ext)s'\'''
+fi
