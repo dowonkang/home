93effd30bc63eb46fa661f87f0cacad51826d774
Dowon Kang <dowon@tuta.io>
2020-03-27 11:15:27 +0900

    Fix AWS environment variable name

--- .config/fish/conf.d/40-aws.fish
+++ .config/fish/conf.d/40-aws.fish
@@ -1,2 +1,2 @@
-set -x AWS_SHARED_CREDENTIAL_FILE "$XDG_CONFIG_HOME/aws/credentials"
+set -x AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME/aws/credentials"
 set -x AWS_CONFIG_FILE "$XDG_CONFIG_HOME/aws/config"
