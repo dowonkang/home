dda072eae95d66eb7aa09bb63b85f64900f3620c
Dowon Kang <dowon@tuta.io>
2020-05-20 15:00:50 +0900

    Add deno completion file for fish shell

--- .config/fish/conf.d/30-deno.fish
+++ .config/fish/conf.d/30-deno.fish
@@ -1,4 +1,11 @@
 if test -d "$HOME/.deno/bin"
     set -x DENO_INSTALL "$HOME/.deno"
     prepend_to_path "$DENO_INSTALL/bin"
+
+    if test ! -e "$HOME/.config/fish/completions/deno.fish"
+        if test ! -e "$HOME/.config/fish/completions"
+            mkdir -p "$HOME/.config/fish/completions"
+        end
+        deno completions fish >"$HOME/.config/fish/completions/deno.fish"
+    end
 end
