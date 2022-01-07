1a152b1f0276a8c92bc42f8b4b5e0f2ac6a073df
Dowon Kang <dev@dowonkang.name>
2022-01-07 14:21:51 +0900

    Add XDG_STATE_HOME

--- .config/fish/conf.d/00-xdg-base-dirs.fish
+++ .config/fish/conf.d/00-xdg-base-dirs.fish
@@ -1,4 +1,5 @@
 # XDG base directories
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 if test ! -d "$HOME/.config"
     mkdir -pv "$HOME/.config"
 end
@@ -7,10 +8,15 @@ if test ! -d "$HOME/.local/share"
     mkdir -pv "$HOME/.local/share"
 end
 
+if test ! -d "$HOME/.local/state"
+    mkdir -pv "$HOME/.local/state"
+end
+
 if test ! -d "$HOME/.cache"
     mkdir -pv "$HOME/.cache"
 end
 
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
--- .config/sh/conf.d/00-xdg-base-dirs.sh
+++ .config/sh/conf.d/00-xdg-base-dirs.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
 # XDG base directories
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 if [ ! -d "$HOME/.config" ]; then
 	mkdir "$HOME/.config"
 fi
 if [ ! -d "$HOME/.local/share" ]; then
 	mkdir -p "$HOME/.local/share"
+fi
+if [ ! -d "$HOME/.local/state" ]; then
+	mkdir -p "$HOME/.local/state"
 fi
 if [ ! -d "$HOME/.cache" ]; then
 	mkdir "$HOME/.cache"
@@ -15,5 +19,7 @@ XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CONFIG_HOME
 XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_HOME
+XDG_STATE_HOME="$HOME/.local/state"
+export XDG_STATE_HOME
 XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CACHE_HOME
