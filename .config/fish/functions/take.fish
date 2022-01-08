a6268436af1cdc279b26d7803dab1c006ca7acb0
Dowon Kang <dev@dowonkang.name>
2022-01-08 13:24:48 +0900

    Fish: take => mkcd

--- .config/fish/functions/take.fish
+++ .config/fish/functions/take.fish
@@ -1,8 +1,6 @@
-# Name is taken from ohmyzsh
-# https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/functions.zsh#L16-L18
-# Code is taken from fishshell documentations
+# Taken from fishshell documentations
 # https://fishshell.com/docs/current/cmds/function.html#example
-function take -d "mkdir then cd"
+function mkcd -d "mkdir then cd"
     command mkdir -p $argv
     if test $status = 0
         switch $argv[(count $argv)]
