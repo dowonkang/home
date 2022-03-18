93a008f207373be1f8cb91b077011a5555f94154
Dowon Kang <dev@dowonkang.name>
2022-03-18 10:37:08 +0900

    remove nvm configs

--- .config/sh/conf.d/30-nodejs.sh
+++ .config/sh/conf.d/30-nodejs.sh
@@ -3,13 +3,8 @@
 
 if [ -d "$HOME/.local/bin" ] && [ -s "$HOME/.local/bin/fnm" ]; then
 	prepend_to_path "$HOME/.local/bin"
-	eval "$(fnm env --use-on-cd)"
-elif [ -d "$HOME/.config/nvm" ] && [ -s "$HOME/.config/nvm/nvm.sh" ]; then
-	NVM_DIR="$HOME/.config/nvm"
-	export NVM_DIR
-	. "$NVM_DIR/nvm.sh"
+fi
 
-	if [ "$BASH" ] && [ -s "$NVM_DIR/bash_completion" ]; then
-		. "$NVM_DIR/bash_completion"
-	fi
+if command -v fnm >/dev/null; then
+	eval "$(fnm env --use-on-cd)"
 fi
