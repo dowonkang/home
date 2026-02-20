60baea1bd84dbc331c0d60d6276c545959d3e834
Dowon Kang <dev@dowonkang.name>
2026-02-20 23:37:56 +0900

    consistent terminal output message

--- .config/fish/conf.d/00-env.fish
+++ .config/fish/conf.d/00-env.fish
@@ -1,2 +1,5 @@
+# consistent terminal output message
+set -gx LC_MESSAGES "C"
+
 # less(1)
 set -gx LESSHISTFILE "$XDG_CACHE_HOME/lesshst"
--- .config/sh/conf.d/00-env.sh
+++ .config/sh/conf.d/00-env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# consistent terminal output message
+LC_MESSAGES=C
+export LC_MESSAGES
+
 # bash(1)
 if [ -n "$BASH" ]; then
 	HISTFILE="$XDG_DATA_HOME/bash_history"
